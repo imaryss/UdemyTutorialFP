904c09cc6f38caf9c6040d5387be256bdb54b782
mariaion <marianaion319@gmail.com>
2022-11-06 16:39:50 +0200

    my first component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import ReactDOM from "react-dom";
 function Greeting() {
   return <h4>this is john and this is my first component</h4>;
 }
+
+ReactDOM.render(<Greeting />, document.getElementById("root"));
